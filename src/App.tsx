417dc70feb5a978f730c79d3289bf4c16227e5ea
Ramdin Sharvesh <ramdin.s@pg.com>
2023-08-28 21:21:42 +0400

    add siteverify to verify token functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
 import React from "react";
 import "./App.css";
 import  ReCAPTCHA  from "react-google-recaptcha";
+import axios from 'axios';
 
 function App() {
   const [isCaptchaSuccessful, setIsCaptchaSuccess] = React.useState(false)
 
+  const handleReCaptchaVerify = (async (token: string) => {
+    // Call Google's API to get score
+    const res = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify?secret=6LeeDeAnAAAAAKyj0IxvVjAKit9IKx_N1RJ_6q8M&response=${token}`
+    );
+    
+    // Extract result from the API response
+    if (res.data.success){
+      console.log('res',res.data)
+      console.log('Valid');
+    } else {
+      console.log('res',res)
+      console.log('Invalid');
+    }
+  })
   function onChange(value: any) {
     setIsCaptchaSuccess(true)
     console.log("captcha value: ", value);
+    handleReCaptchaVerify(value);
+    
   }
 
   return (
     <>
-      <form>
+      <form method="post">
         <input id="username" name="username" placeholder="Username" />
         <input
           type="password"
@@ -23,8 +41,8 @@ function App() {
         <ReCAPTCHA
           sitekey="6LeeDeAnAAAAAIk95GzBZNxUqyeHC07mmym2W9cU"
           onChange={onChange}
-          />
-          <button disabled={!isCaptchaSuccessful}>Login</button>
+        />
+        <button disabled={!isCaptchaSuccessful}>Login</button>
       </form>
     </>
   );
