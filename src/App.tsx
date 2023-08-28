a2a03dc80bacadb17ea3956fd96248ed2e70c9cc
Ramdin Sharvesh <ramdin.s@pg.com>
2023-08-28 19:57:55 +0400

    add recaptcha v2 implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import  ReCAPTCHA  from "react-google-recaptcha";
 
 function App() {
+  const [isCaptchaSuccessful, setIsCaptchaSuccess] = React.useState(false)
+
+  function onChange(value: any) {
+    setIsCaptchaSuccess(true)
+    console.log("captcha value: ", value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <input id="username" name="username" placeholder="Username" />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+        />
+        <ReCAPTCHA
+          sitekey="6LeeDeAnAAAAAIk95GzBZNxUqyeHC07mmym2W9cU"
+          onChange={onChange}
+          />
+          <button disabled={!isCaptchaSuccessful}>Login</button>
+      </form>
+    </>
   );
 }
 
